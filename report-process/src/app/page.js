ffb911115624246be28c734a54905e3f65506e46
NOOK <noompakonnoo@gmail.com>
2023-11-29 10:57:53 +0700

    commit

--- report-process/src/app/page.js
+++ report-process/src/app/page.js
@@ -1,14 +1,34 @@
+"use client"
+import React, { useState } from "react";
+import { useEdgeStore } from "@/lib/edgestore";
 
-export default function Home() {
+export default function Page() {
+
+    const [file, setFile] = useState();
+    const [urls, setUrls] = useState<{
+        url: stringifyCookie;
+        thumbnailUrl: string | null;
+    }>();
+    const { edgestore } = useEdgeStore();
     return (
-        <div className="grid m-4">
-            <div className='col'>
-                <h1 className="text-3xl font-bold underline">
-                    Report NSW
-                </h1>
-            </div>
-            <div className='col'>
-            </div>
+        <div className="flex flex-col items-center m-6 gap-2">
+            <input
+                type="file"
+                onChange={(e) => {
+                    setFile(e.target.files?.[0]);
+                }}
+            />
+            <button
+                className="bg-white text-block rounded px-2 hover:opacity-80"
+                onClick={async () => {
+                    if(file) {
+                        const res = await edgestore.myPublicImages.upload({file});
+                        // save your data here
+                    }
+                }}
+            >
+                Upload
+            </button>
         </div>
     )
 }

38701ec0b0c668d0a9c8232196b071c7bdf5eec6
NOOK <noompakonnoo@gmail.com>
2023-11-29 14:38:54 +0700

    Update page.js

--- report-process/src/app/page.js
+++ report-process/src/app/page.js
@@ -1,34 +1,39 @@
-"use client"
-import React, { useState } from "react";
-import { useEdgeStore } from "@/lib/edgestore";
+import React from "react";
 
 export default function Page() {
 
-    const [file, setFile] = useState();
-    const [urls, setUrls] = useState<{
-        url: stringifyCookie;
-        thumbnailUrl: string | null;
-    }>();
-    const { edgestore } = useEdgeStore();
-    return (
-        <div className="flex flex-col items-center m-6 gap-2">
-            <input
-                type="file"
-                onChange={(e) => {
-                    setFile(e.target.files?.[0]);
-                }}
-            />
-            <button
-                className="bg-white text-block rounded px-2 hover:opacity-80"
-                onClick={async () => {
-                    if(file) {
-                        const res = await edgestore.myPublicImages.upload({file});
-                        // save your data here
-                    }
-                }}
-            >
-                Upload
-            </button>
+
+  return (
+    <div className="md:container md:mx-auto">
+      <h1 className="text-2xl font-bold text-black mb-7"> Files</h1>
+      <div className="space-x-3">
+        <div className="inline-block">
+          <button
+            type="button"
+            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-1 me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+            Refresh
+          </button>
         </div>
-    )
+        <div className="inline-block">
+          <label
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+            htmlFor="file_input">
+            Upload file
+          </label>
+          <input
+            className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            id="file_input"
+            type="file"
+          />
+        </div>
+        <div className="inline-block">
+          <button
+            type="button"
+            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-1 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+            Upload
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }

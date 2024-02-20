e106bebf729539ee0aaa1c7c87e7f407f5989bbc
chanchat.t@kabintech.co.th <Chanchat.t@kabintech.co.th>
2024-02-20 23:51:32 +0700

    commit2

--- report-process-typescript/src/app/api/upload3/route.ts
+++ report-process-typescript/src/app/api/upload3/route.ts
@@ -1,11 +1,38 @@
 import { NextResponse } from "next/server"
+import fs from 'fs/promises'
+import path from 'path';
 
 export async function POST(req: Request, res: Response){
     const reqeust = await req.formData();
-
-
     return NextResponse.json({
         status: 200,
         message: 'sus'
     })
+}
+
+export async function GET(req: Request, res: Response){
+    const dir =  path.resolve('./assits/uploads' );
+    try{
+        const filenames = await fs.readdir(dir) ? await fs.readdir(dir) : [];
+        console.log(filenames)
+        const filesDetail = filenames.length > 0 ? await Promise.all(filenames.map(async filename => {
+            const filePath = path.join(dir, filename);
+            const detail = await fs.stat(filePath);
+                return {
+                    name: filename,
+                    path: filePath,
+                    size: detail.size, 
+                    modifiedAt: detail.mtime 
+                }
+            })) : [] ;
+        return NextResponse.json({
+            status: 200,
+            data: filesDetail
+        })        
+    } catch(err){
+        return NextResponse.json({
+            status: 500,
+            message: 'Internal Server Error'
+        })        
+    }
 }
--- report-process-typescript/src/app/page.tsx
+++ report-process-typescript/src/app/page.tsx
@@ -1,10 +1,15 @@
 'use client'
 
 import { useState } from 'react';
-import { submitUpload } from './actions';
+
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import ClearIcon from '@mui/icons-material/Clear';
-import fs from 'fs/promises';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export default function Home() {
   const [file, setFile] = useState<File | null>();
@@ -27,12 +32,31 @@ export default function Home() {
     }
   }
 
+  function createData(
+    name: string,
+    calories: number,
+    fat: number,
+    carbs: number,
+    protein: number,
+  ) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+  const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ];
+
+
   return (
     <main>
       <div className="max-w-lg rounded overflow-hidden shadow-lg m-4 p-4 bg-[#94a3b8]">
         <div className="mb-4 p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
           <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">!</span>
-          <span className="font-semibold mr-2 text-left flex-auto">อัพโหลดแบบที่ 1</span>
+          <span className="font-semibold mr-2 text-left flex-auto">อัพโหลด</span>
           <ArrowForwardIosIcon fontSize='small'></ArrowForwardIosIcon>
         </div>
         <form onSubmit={onSubmit}>
@@ -49,11 +73,27 @@ export default function Home() {
           />
         </form>
       </div>
-      <div>
-          <table>
-          
-          </table>
-        </div>
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">File Name</TableCell>
+            <TableCell align="center">File sizes (byte)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="center">{row.calories}</TableCell>
+              <TableCell align="center">{row.fat}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     </main>
   )
 }

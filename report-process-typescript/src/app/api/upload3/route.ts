7cf2c3c651b414058bf8987f9d51e7481a66834b
chanchat.t@kabintech.co.th <chanchat.t@kabintech.co.th>
2024-02-21 16:25:15 +0700

    -list file

--- report-process-typescript/src/app/api/upload3/route.ts
+++ report-process-typescript/src/app/api/upload3/route.ts
@@ -1,20 +1,23 @@
 import { NextResponse } from "next/server"
 import fs from 'fs/promises'
 import path from 'path';
+import * as XLSX from 'xlsx';
 
 export async function POST(req: Request, res: Response){
-    const reqeust = await req.formData();
-    return NextResponse.json({
-        status: 200,
-        message: 'sus'
-    })
+    try{
+        console.log(req);
+    } catch(err){
+        return NextResponse.json({
+            status: 200,
+            message: 'sus'
+        })
+    }
 }
 
 export async function GET(req: Request, res: Response){
     const dir =  path.resolve('./assits/uploads' );
     try{
         const filenames = await fs.readdir(dir) ? await fs.readdir(dir) : [];
-        console.log(filenames)
         const filesDetail = filenames.length > 0 ? await Promise.all(filenames.map(async filename => {
             const filePath = path.join(dir, filename);
             const detail = await fs.stat(filePath);
--- report-process-typescript/src/app/page.tsx
+++ report-process-typescript/src/app/page.tsx
@@ -1,99 +1,99 @@
-'use client'
+"use client";
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 export default function Home() {
   const [file, setFile] = useState<File | null>();
-  const handleClearFile = () => {setFile(null)};
-  const pathToSave = '/assets/files/';
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const pathToSave = "/assets/files/";
+  const rows = [
+    // createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    // createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    // createData("Eclair", 262, 16.0, 24, 6.0),
+    // createData("Cupcake", 305, 3.7, 67, 4.3),
+    // createData("Gingerbread", 356, 16.0, 49, 3.9),
+  ];
+  const [rowss, setRows] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchingData();
+  },[])
+
+  const fetchingData = async() => {
+    try{
+      const res = await fetch("/api/upload3",{
+        method: "GET"
+      });
+      const data = await res.json();
+      setRows(data['data']);
+    }catch(err){
+      console.log(err);
+    }
+  };
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    if (!file) return
+    e.preventDefault();
+    if (!file) return;
     try {
       const data = new FormData();
-      data.set('file', file);
-      const res = await fetch('/api/upload', {
-        method: 'POST',
-        body: data
+      data.set("file", file);
+      const res = await fetch("/api/upload", {
+        method: "POST",
+        body: data,
       });
-      console.log(res);
+      if(await res.json()){
+        await fetchingData();
+        setFile(null);
+        if (fileInputRef.current) fileInputRef.current.value = '';
+      }
     } catch (e: any) {
       console.error(e);
     }
-  }
-
-  function createData(
-    name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number,
-  ) {
-    return { name, calories, fat, carbs, protein };
-  }
-
-  const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];
+  };
 
 
   return (
     <main>
-      <div className="max-w-lg rounded overflow-hidden shadow-lg m-4 p-4 bg-[#94a3b8]">
+      <div className="max-w-lg rounded overflow-hidden shadow-lg m-4 p-4 bg-[#94a3b8] mx-auto">
         <div className="mb-4 p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
           <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">!</span>
           <span className="font-semibold mr-2 text-left flex-auto">อัพโหลด</span>
-          <ArrowForwardIosIcon fontSize='small'></ArrowForwardIosIcon>
+          <ArrowForwardIosIcon fontSize="small"></ArrowForwardIosIcon>
         </div>
-        <form onSubmit={onSubmit}>
-          <input
-            type="file"
-            name="file"
-            onChange={(e) => setFile(e.target.files?.[0])}
-            className='mb-4'
-          />
-          <input
-            className='ring-2 px-3 py-2 bg-blue-800 text-white rounded-md cursor-pointer' 
-            type="submit"
-            value="Upload"
-          />
+        <form onSubmit={onSubmit} className="flex justify-between items-center">
+          <input type="file" name="file" onChange={(e) => setFile(e.target.files?.[0])}  ref={fileInputRef}/>
+          <input className="ring-2 px-3 py-2 bg-blue-800 text-white rounded-md cursor-pointer text-end" type="submit" value="Upload" />
         </form>
       </div>
-      <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">File Name</TableCell>
-            <TableCell align="center">File sizes (byte)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell align="center">{row.calories}</TableCell>
-              <TableCell align="center">{row.fat}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+      <div className="max-w-lg">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 200 }} aria-label="simple table">
+            <TableHead>
+              <TableRow className="bg-[#94a3b8]">
+                <TableCell align="center">File Name</TableCell>
+                <TableCell align="center">File sizes (byte)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rowss.map((row) => (
+                <TableRow key={row.name} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                  <TableCell align="center">{row.name}</TableCell>
+                  <TableCell align="center">{row.size}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
     </main>
-  )
+  );
 }
